a34c1a3d6ca0a333758157db49e114def54616b6
Julián Osiniri <jotaosiniri@gmail.com>
2026-03-13 22:54:14 +0100

    menu.h y menu.cpp

--- src/dibujar.cpp
+++ src/dibujar.cpp
@@ -4,6 +4,7 @@
 
 void Dibujar(const char* rutaImagen, float posx, float posy, float posz, float ancho, float alto)
 {
+
 	glEnable(GL_TEXTURE_2D);
 
 	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
@@ -15,17 +16,16 @@ void Dibujar(const char* rutaImagen, float posx, float posy, float posz, float a
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-
 	glDisable(GL_LIGHTING);
 	glBegin(GL_POLYGON);
 	glColor3f(1, 1, 1);
-	glTexCoord2d(0.0, 1.0);    glVertex3f(posx, posy, posz);  // Abajo izquierda
-	glTexCoord2d(1.0, 1.0);    glVertex3f(posx + ancho, posy, posz);   // Abajo derecha
-	glTexCoord2d(1.0, 0.0);    glVertex3f(posx + ancho, posy + alto, posz);    // Arriba derecha
-	glTexCoord2d(0.0, 0.0);    glVertex3f(posx, posy + alto,posz);   // Arriba izquierda
+	glTexCoord2d(0, 1);    glVertex3f(posx, posy, posz);  // Abajo izquierda
+	glTexCoord2d(1, 1);    glVertex3f(posx + ancho, posy, posz);   // Abajo derecha
+	glTexCoord2d(1, 0);    glVertex3f(posx + ancho, posy + alto, posz);    // Arriba derecha
+	glTexCoord2d(0, 0);    glVertex3f(posx, posy + alto,posz);   // Arriba izquierda
 
-	// Estas funciones ponen la posicion x e y de la imagen en su esquina inferior izq. Además suponen que la imagen es un 
-	// cuadrado de lado tam, lo que hay que solucionar cambiando los parametros al pedir un dibujo desde main con Dibujar().
+	// Estas funciones ponen la posicion x e y de la imagen en su esquina inferior izq. 
+	// SOLUCIONADO: Además suponen que la imagen es un cuadrado de lado tam, lo que hay que solucionar cambiando los parametros al pedir un dibujo desde main con Dibujar().
 
 	glEnd();
 	//glEnable(GL_LIGHTING);
--- src/menu.cpp
+++ src/menu.cpp
@@ -5,9 +5,12 @@
 void  mostrarMenu()
 {
 
-Dibujar("../assets/Sprites/Pruebas/Sprite-0003.png", 0, 0, -20,50,50);
-Dibujar("../assets/Sprites/Pruebas/Sprite-0004.png", 0, 0, -21, 50, 50);
-Dibujar("../assets/Sprites/Pruebas/Sprite-0005.png", 30, 30, -22, 50, 50);
+//Dibujar("../assets/Sprites/Pruebas/Sprite-0003.png", 0, 0, -20,50,50);
+//Dibujar("../assets/Sprites/Pruebas/Sprite-0004.png", 0, 0, -21, 50, 50);
+//Dibujar("../assets/Sprites/Pruebas/Sprite-0005.png", 30, 30, -22, 50, 50);
+
+	//Dibujar("../assets/Sprites/menu/fondo1.png", 0, 0, 3, 160, 90);
+
 return;
 }
 
