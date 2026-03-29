a916f14c336086784a54e108cf6528ee41b5b1b1
Julián Osiniri <jotaosiniri@gmail.com>
2026-03-29 22:23:08 +0200

    Corección de capa a 0

--- src/renderizador.cpp
+++ src/renderizador.cpp
@@ -26,10 +26,10 @@ void Renderizador::dibujarSprite(const char* rutaImagen, float ancho, float alto
     glPushMatrix();  // El renderizador se encarga de mover el origen antes de dibujar en el (0,0)
     glTranslatef(posx, posy, capa); // ahora posicion (x,y) en su capa (capa) corresponden a centro de imagen
     glBegin(GL_POLYGON);
-    glTexCoord2d(0, 1);    glVertex3f( - ancho / 2,  - alto / 2, capa); // abajo izquierda
-    glTexCoord2d(1, 1);    glVertex3f(  ancho / 2,  - alto / 2, capa); // abajo derecha
-    glTexCoord2d(1, 0);    glVertex3f(  ancho / 2,   alto / 2, capa); // arriba derecha
-    glTexCoord2d(0, 0);    glVertex3f( - ancho / 2,   alto / 2, capa); // ariiba izquierda
+    glTexCoord2d(0, 1);    glVertex3f( - ancho / 2,  - alto / 2, 0); // abajo izquierda
+    glTexCoord2d(1, 1);    glVertex3f(  ancho / 2,  - alto / 2, 0); // abajo derecha
+    glTexCoord2d(1, 0);    glVertex3f(  ancho / 2,   alto / 2, 0); // arriba derecha
+    glTexCoord2d(0, 0);    glVertex3f( - ancho / 2,   alto / 2, 0); // ariiba izquierda
     glEnd();
     glPopMatrix();
 
